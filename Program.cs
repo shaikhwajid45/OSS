91d2c1896ea11de0af0eda83b07f3c5fe73efb01
ShravaniKaranjkar <108930171+ShravaniKaranjkar@users.noreply.github.com>
2022-07-08 15:45:38 +0530

    this is uploaded file

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your First name");
+            String fname = Console.ReadLine();
+            Console.WriteLine("Enter your last name");
+            String lname = Console.ReadLine();
+
+            Console.ReadLine();
+        }
+    }
+}
